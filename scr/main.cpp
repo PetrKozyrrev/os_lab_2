bc100de18e76e99303a5293eca9fb89cf31dc276
Petr <peterrrkozyrrrev@gmail.com>
2023-10-12 18:08:56 +0300

    update

--- scr/main.cpp
+++ scr/main.cpp
@@ -3,38 +3,44 @@
 #include <vector>
 #include <fstream>
 #include <cmath>
-#include <thread>
+#include <pthread.h>
 #include <mutex>
 #include <chrono>
 #include "bigint.h"
 
-std::mutex mutex;
+
+typedef struct someArgs_tag {
+    int id;
+    int start;
+    int finish;
+} someArgs_t;
+
 
 big_integer from_16_to_10(std::string &arr){
     big_integer res = 0;
-    for(int i=0;i<arr.size();++i){
-        if(arr[i]>='0' and arr[i]<='9'){
+    for(int i = 0; i < arr.size(); ++i){
+        if(arr[i] >= '0' and arr[i] <= '9'){
             big_integer st = 16;
             st = st.pow(arr.size()-i-1);
-            st*= ((int)(arr[i]-'0'));
-            res+=st;
+            st *= ((int)(arr[i]-'0'));
+            res += st;
         }
         else{
             int d;
             switch (arr[i])
             {
-            case 'A': d = 10; break;
-            case 'B': d = 11; break;
-            case 'C': d = 12; break;
-            case 'D': d = 13; break;
-            case 'E': d = 14; break;
-            case 'F': d = 15; break;
-            default: throw std::string("Input error");
+                case 'A': d = 10; break;
+                case 'B': d = 11; break;
+                case 'C': d = 12; break;
+                case 'D': d = 13; break;
+                case 'E': d = 14; break;
+                case 'F': d = 15; break;
+                default: throw std::string("Input error");
             }
             big_integer st = 16;
             st = st.pow(arr.size()-i-1);
-            st*= d;
-            res+=st;
+            st *= d;
+            res += st;
         }
     }
     return res;
@@ -42,13 +48,13 @@ big_integer from_16_to_10(std::string &arr){
 
 big_integer sm = 0;
 std::vector<big_integer> array_numbers;
+std::vector<std::string> array_str;
 
-void sum_array(int th_id,int step){
-    int index = th_id*step;
-    for(int i=index;i<index+step;++i){
-        if(i<array_numbers.size())
-            //std::cout << th_id << " " << i << std::endl;
-            sm+=array_numbers[i];
+void* sum_array(void* args){
+    someArgs_t *arg = (someArgs_t*) args;
+    for(int i = arg->start; i < arg->finish; ++i){
+        if(i < array_str.size())
+            sm += from_16_to_10(array_str[i]);
     }
     pthread_exit(0);
 }
@@ -64,17 +70,18 @@ int main(int argc,char* argv[]){
 
     const int threadCount = atoi(argv[1]); // количество потоков
 
-    std::thread t[threadCount]; 
+    pthread_t t[threadCount];
+
 
     std::string line;
-    std::vector<std::string> array_str;
+    
     
     std::string path = "../../test/";
     std::string name;
     std::cout << "Input test file name: ";
-    std::cin >>name;
+    std::cin >> name;
     std::cout << std::endl; 
-    path+=name;
+    path += name;
     std::ifstream file(path);
 
     if (file.is_open()){
@@ -82,32 +89,36 @@ int main(int argc,char* argv[]){
             array_str.push_back(line);
         }
     }
+
     file.close();
 
     int arr_size = array_str.size();
-    int step = (arr_size+(arr_size%threadCount))/threadCount + 1;
+    int step = ((arr_size) /threadCount) + 1;
 
     if(arr_size == 0){
         std::cerr << "Empty file\n";
         return 1;
     }
 
-    for(auto elem: array_str){
-        try{
-            array_numbers.push_back(from_16_to_10(elem));
-        }
-        catch(std::string error_message){
-            std::cout << error_message << std::endl;
-            return 1;
-        }
+    someArgs_t args[threadCount];
+
+    for(int i{0};i<threadCount;++i){
+        args[i].id = i;
+
+        if(i == 0)
+            args[i].start = 0;
+        else
+            args[i].start = i*step;
+
+        args[i].finish = (i+1)*step ;
     }
 
     for(int i{0};i<threadCount;++i){
-        t[i] = std::thread(sum_array,i,step);
+        pthread_create(&t[i], NULL, sum_array, (void*)&args[i]);
     }
 
     for(int i{0};i<threadCount;++i){
-        t[i].join();
+        pthread_join(t[i],NULL);
     }
 
     big_integer averg = sm/arr_size;
